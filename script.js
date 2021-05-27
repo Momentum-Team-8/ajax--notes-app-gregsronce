b9fe965ed5d9d042ce600f40de4cea5626c1c7a8
Greg Sronce <greg.sronce@gmail.com>
2021-05-27 19:42:10 -0400

    Experimenting with POST request and Jeanette's example.

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const url =  'http://localhost:3000/notes'
+const form = document.querySelector('#notes-form')
+const notesList = document.querySelector('#notes-list')
+const submitButton = document.getElementById('submit-button')
 
-const root = document.querySelector('#container')
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const noteText= document.getElementById('notes-input').value
+    fetch(url, {
+        method: 'POST',
+        header: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+            title: noteText,
+            body: noteText,
+            create_at: moment().format()
+        })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
 
-
+function listNotes() {
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-    console.log(data)
-    const notes = document.createElement('h1')
-    root.appendChild(notes)
-    notes.innerText = `${data[0].title} \n \n ${data[0].body}`
+    for (let note of data) {
+        console.log(note)
+        // renderNoteItem(note)
+ }
 })
+}
+
